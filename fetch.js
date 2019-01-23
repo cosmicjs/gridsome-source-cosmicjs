e957f8532a6e1b4170bfcab556100f416760e9c9
Jazib Sawar <jazibsawar@gmail.com>
2019-01-24 00:50:38 +0500

    Removed babelrc

--- fetch.js
+++ fetch.js
@@ -1,79 +1,29 @@
-'use strict';
+const axios = require('axios')
+const { isObject, startsWith, forEach } = require('lodash')
 
-var _regenerator = require('babel-runtime/regenerator');
+module.exports = async ({ apiURL, bucketSlug, objectType, apiAccess }) => {
+  console.time('Fetch Cosmic JS data')
+  console.log(`Starting to fetch data from Cosmic JS (${objectType})`)
 
-var _regenerator2 = _interopRequireDefault(_regenerator);
+  // Define API endpoint.
+  let apiEndpoint = `${apiURL}/${bucketSlug}/objects?type=${objectType}`
 
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+  if (apiAccess.hasOwnProperty('read_key') && apiAccess.read_key.length !== 0) {
+    apiEndpoint = apiEndpoint + `&read_key=${apiAccess.read_key}`
+  }
+  // Make API request.
+  const documents = await axios(apiEndpoint)
 
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+  // Query all data from endpoint
+  console.timeEnd('Fetch Cosmic JS data')
 
-var _axios = require('axios');
-
-var _axios2 = _interopRequireDefault(_axios);
-
-var _lodash = require('lodash');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = function () {
-  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_ref2) {
-    var apiURL = _ref2.apiURL,
-        bucketSlug = _ref2.bucketSlug,
-        objectType = _ref2.objectType,
-        apiAccess = _ref2.apiAccess;
-    var apiEndpoint, documents;
-    return _regenerator2.default.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            console.time('Fetch Cosmic JS data');
-            console.log('Starting to fetch data from Cosmic JS (' + objectType + ')');
-
-            // Define API endpoint.
-            apiEndpoint = apiURL + '/' + bucketSlug + '/objects?type=' + objectType;
-
-
-            if (apiAccess.hasOwnProperty('read_key') && apiAccess.read_key.length !== 0) {
-              apiEndpoint = apiEndpoint + ('&read_key=' + apiAccess.read_key);
-            }
-            // Make API request.
-            _context.next = 6;
-            return (0, _axios2.default)(apiEndpoint);
-
-          case 6:
-            documents = _context.sent;
-
-
-            // Query all data from endpoint
-            console.timeEnd('Fetch Cosmic JS data');
-
-            // Map and clean data.
-
-            if (!documents.data.objects) {
-              _context.next = 12;
-              break;
-            }
-
-            return _context.abrupt('return', documents.data.objects.map(function (item) {
-              return clean(item);
-            }));
-
-          case 12:
-            return _context.abrupt('return', []);
-
-          case 13:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, undefined);
-  }));
-
-  return function (_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
+  // Map and clean data.
+  if (documents.data.objects) {
+    return documents.data.objects.map(item => clean(item))
+  } else {
+    return []
+  }
+}
 
 /**
  * Remove fields starting with `_` symbol.
@@ -81,14 +31,14 @@ module.exports = function () {
  * @param {object} item - Entry needing clean
  * @returns {object} output - Object cleaned
  */
-var clean = function clean(item) {
-  (0, _lodash.forEach)(item, function (value, key) {
-    if ((0, _lodash.startsWith)(key, '__')) {
-      delete item[key];
-    } else if ((0, _lodash.isObject)(value)) {
-      item[key] = clean(value);
+const clean = item => {
+  forEach(item, (value, key) => {
+    if (startsWith(key, `__`)) {
+      delete item[key]
+    } else if (isObject(value)) {
+      item[key] = clean(value)
     }
-  });
+  })
 
-  return item;
-};
+  return item
+}
--- index.js
+++ index.js
@@ -1,129 +1,62 @@
-'use strict';
+const fetchData = require('./fetch')
+const { capitalize } = require('lodash')
 
-var _regenerator = require('babel-runtime/regenerator');
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
-var _promise = require('babel-runtime/core-js/promise');
-
-var _promise2 = _interopRequireDefault(_promise);
-
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
-
-var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
-
-var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-
-var _createClass2 = require('babel-runtime/helpers/createClass');
-
-var _createClass3 = _interopRequireDefault(_createClass2);
-
-var _fetch = require('./fetch');
-
-var _fetch2 = _interopRequireDefault(_fetch);
-
-var _lodash = require('lodash');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var CosmicJsSource = function () {
-  (0, _createClass3.default)(CosmicJsSource, null, [{
-    key: 'defaultOptions',
-    value: function defaultOptions() {
-      return {
+class CosmicJsSource {
+  static defaultOptions() {
+    return {
         typeName: 'Cosmicjs',
         apiURL: 'https://api.cosmicjs.com/v1',
-        bucketSlug: '',
+        bucketSlug:'',
         objectTypes: [],
-        apiAccess: {}
-      };
-    }
-  }]);
-
-  function CosmicJsSource(api, options) {
-    var _this = this;
-
-    (0, _classCallCheck3.default)(this, CosmicJsSource);
-
-    this.options = options;
-    api.loadSource(function (args) {
-      return _this.fetchContent(args);
-    });
+        apiAccess: {},
+    };
   }
 
-  (0, _createClass3.default)(CosmicJsSource, [{
-    key: 'fetchContent',
-    value: function () {
-      var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(store) {
-        var addContentType, _options, typeName, apiURL, bucketSlug, objectTypes, apiAccess, promises, data;
+  constructor(api, options) {
+    this.options = options;
+    api.loadSource(args => this.fetchContent(args));
+  }
 
-        return _regenerator2.default.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                addContentType = store.addContentType;
-                _options = this.options, typeName = _options.typeName, apiURL = _options.apiURL, bucketSlug = _options.bucketSlug, objectTypes = _options.objectTypes, apiAccess = _options.apiAccess;
-                promises = objectTypes.map(function (objectType) {
-                  return (0, _fetch2.default)({
-                    apiURL: apiURL,
-                    bucketSlug: bucketSlug,
-                    objectType: objectType,
-                    apiAccess: apiAccess
-                  });
-                });
-                _context.next = 5;
-                return _promise2.default.all(promises);
+  async fetchContent(store) {
+    const { addContentType } = store;
+    const { typeName, apiURL, bucketSlug, objectTypes, apiAccess } = this.options;
 
-              case 5:
-                data = _context.sent;
+    const promises = objectTypes.map(objectType =>
+      fetchData({
+        apiURL,
+        bucketSlug,
+        objectType,
+        apiAccess,
+      })
+    )
 
+    const data = await Promise.all(promises)
 
-                objectTypes.forEach(function (objectType, i) {
-                  var contentType = addContentType({
-                    typeName: '' + typeName + (0, _lodash.capitalize)(objectType)
-                  });
-                  var items = data[i];
-                  items.forEach(function (item, index) {
-                    var node = {
-                      id: item._id,
-                      title: item.title,
-                      slug: item.slug || '',
-                      date: item.created_at,
-                      content: item.content,
-                      path: objectType + '/' + item.slug,
-                      fields: (0, _extends3.default)({
-                        nextPath: index < items.length - 1 ? objectType + '/' + items[index + 1].slug : null,
-                        prevPath: index > 0 ? objectType + '/' + items[index - 1].slug : null,
-                        nextTitle: index < items.length - 1 ? '' + items[index + 1].title : null,
-                        prevTitle: index > 0 ? '' + items[index - 1].title : null
-                      }, item)
-                    };
-                    contentType.addNode(node);
-                  });
-                });
-
-              case 7:
-              case 'end':
-                return _context.stop();
-            }
+    objectTypes.forEach((objectType, i) => {
+      const contentType = addContentType({
+        typeName: `${typeName}${capitalize(objectType)}`,
+      });
+      var items = data[i]
+      items.forEach((item, index) => {
+        const node = {
+          id: item._id,
+          title: item.title,
+          slug: item.slug || '',
+          date: item.created_at,
+          content: item.content,
+          path: `${objectType}/${item.slug}`,
+          fields: {
+            nextPath: index < (items.length - 1) ? `${objectType}/${items[index + 1].slug}`: null,
+            prevPath: index > 0 ? `${objectType}/${items[index - 1].slug}`: null,
+            nextTitle: index < (items.length - 1) ? `${items[index + 1].title}`: null,
+            prevTitle: index > 0 ? `${items[index - 1].title}`: null,
+            ...item
           }
-        }, _callee, this);
-      }));
-
-      function fetchContent(_x) {
-        return _ref.apply(this, arguments);
-      }
-
-      return fetchContent;
-    }()
-  }]);
-  return CosmicJsSource;
-}();
+        }
+        contentType.addNode(node)
+      })
+    })
+  }
+}
 
 module.exports = CosmicJsSource;
